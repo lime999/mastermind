b39b917031228d1246243f31f216178f9a8a6538
Emil.N <emil.k.neitzke@gmail.com>
2022-05-15 14:26:00 +0200

    create addRow, addButton

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ let colors = [
     "orange"
 ]
 function selectColor11() {
-    let firstRowOne = document.getElementById("firstRow1")
+    let firstRowOne = document.getElementById("1_1")
     console.log(firstRowOne.style.backgroundColor)
     console.log(firstRowOne.style)
     firstRowOne.style.backgroundColor = getNextColor(firstRowOne.style.backgroundColor)
 }
+
 function getNextColor(currentColor) {
     if (currentColor === "grey") {
         return colors[0]
@@ -22,3 +23,20 @@ function getNextColor(currentColor) {
     let index = colors.indexOf(currentColor)
     return colors[index + 1]
 }
+
+function addRow() {
+    let board = document.getElementById("board")
+    let row = document.createElement("div")
+    board.appendChild(row)
+    row.className = "row"
+    addButton(row)
+}
+
+function addButton(row) {
+    let button = document.createElement("button")
+    button.className = "hole"
+    button.id = "1_1"
+    button.onclick = selectColor11
+    row.appendChild(button)
+}
+addRow()
