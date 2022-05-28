db607b05edfde8273c08e38bce8de6c6d76959a5
Emil.N <emil.k.neitzke@gmail.com>
2022-05-28 11:15:47 +0200

    styling pins and adding headline

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ let colors = [
 function selectButtonColor(buttonId) {
     let button = document.getElementById(buttonId)
     button.style.backgroundColor = getNextColor(button.style.backgroundColor)
+    button.classList.remove("hole")
+    button.classList.add("pin")
 }
 
 function getNextColor(currentColor) {
@@ -29,20 +31,25 @@ function addRow(rowId) {
     row.className = "row"
     row.id = "row_" + rowId
     for (let i = 0; i < 4; i++) {
-        addButton(row,rowId, i + 1)
+        addButton(row, rowId, i + 1)
     }
 }
 
 function addButton(row, rowId, buttonId) {
+    let buttonWrapper =document.createElement("div")
     let button = document.createElement("button")
+    buttonWrapper.appendChild(button)
+    buttonWrapper.className = "button-wrapper"
     button.className = "hole"
     button.id = "button_" + rowId + "_" + buttonId
     button.addEventListener('click', function () {
         selectButtonColor(button.id)
     });
-    row.appendChild(button)
+    row.appendChild(buttonWrapper)
 }
 
-for (let i = 0; i < 9; i++) {
-    addRow(i + 1)
+window.onload = function () {
+    for (let i = 0; i < 9; i++) {
+        addRow(i + 1)
+    }
 }
