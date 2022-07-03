779ff4cdf89fd8c7bfb7ac8c3434c03a638c1526
Emil.N <emil.k.neitzke@gmail.com>
2022-07-03 12:21:05 +0200

    popup color selector

--- examples/popup.js
+++ examples/popup.js
@@ -1,4 +1,18 @@
+let colors = [
+    "red",
+    "lightblue",
+    "white",
+    "yellow",
+    "lightgreen",
+    "pink",
+    "silver",
+    "orange",
+];
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+}
+function selectPinColor(pinId, color) {
+    let pin = document.getElementById(pinId);
+    pin.style.backgroundColor = color
 }
