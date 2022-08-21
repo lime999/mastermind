886df5ea8f8739cb5b1c0b7d89a1a49b657620c2
Emil.N <emil.k.neitzke@gmail.com>
2022-08-21 13:03:53 +0200

    refactor popup

--- examples/popup.js
+++ examples/popup.js
@@ -9,11 +9,31 @@ let colors = [
     "orange",
     "gray"
 ];
-function myFunction() {
-    var popup = document.getElementById("myPopup");
+function showPopup(pinId) {
+    let popup = document.getElementById("colorSelect");
     popup.classList.toggle("show");
+    addSelectPin(pinId)
 }
 function selectPinColor(pinId, color) {
     let pin = document.getElementById(pinId);
     pin.style.backgroundColor = color
+}
+function addSelectPin(pinId) {
+    let pinWrapper = document.createElement("div");
+    let pin = createSelectPin(pinId);
+    pinWrapper.appendChild(pin);
+    pinWrapper.className = "pin-wrapper";
+    let popup = document.getElementById("colorSelect")
+    popup.appendChild(pinWrapper);
+}
+function createSelectPin(pinId) {
+    let selectPin = document.createElement("div");
+    selectPin.classList.add("pin-position");
+    selectPin.classList.add("pin");
+    selectPin.id = "color-red";
+    selectPin.addEventListener("click", function () {
+        selectPinColor(pinId, "red")
+    })
+    selectPin.style.backgroundColor = "red"
+    return selectPin;
 }
